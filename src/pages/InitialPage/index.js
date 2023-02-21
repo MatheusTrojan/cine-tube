2d29549b514160330e172c662a0b557221fd51ad
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-21 14:24:52 -0300

    create a card for each trailer in json file!

--- src/pages/InitialPage/index.js
+++ src/pages/InitialPage/index.js
@@ -1,8 +1,10 @@
+import styles from "./InitialPage.module.css"
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
+import videos from "json/db.json"
 
 function InitialPage() {
    return (
@@ -13,7 +15,11 @@ function InitialPage() {
             <h1>Assista aqui os novos trailers de seus filmes favoritos!</h1>
          </Title>
 
-         <Card id="1" title="teste" coverImage="https://i.ytimg.com/vi/a1w9x5U88jU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBH0ZkE9Az01m7D2Clso2EV9Xg2J" />
+         <section className={styles.container}>
+            {videos.map((video) => {
+               return <Card {...video} key={video.id}/>
+            })}
+         </section>
 
          <Footer />
       </>
