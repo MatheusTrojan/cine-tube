e7073a51a702d649b000f653a97acd70e85c44fd
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-21 10:25:06 -0300

    create title component

--- src/pages/InitialPage/index.js
+++ src/pages/InitialPage/index.js
@@ -1,15 +1,19 @@
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Banner from "components/Banner";
+import Title from "components/Title";
 
 function InitialPage() {
-  return (
-    <>
-      <Header />
-      <Banner image="home"/>
-      <Footer />
-    </>
-  )
+   return (
+      <>
+         <Header />
+         <Banner image="home"/>
+         <Title>
+            <h1>Assista aqui os novos trailers de seus filmes favoritos!</h1>
+         </Title>
+         <Footer />
+      </>
+   )
 }
 
 export default InitialPage;
