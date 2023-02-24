9464917a3137ce7b364a6bc90828716fbf89c66e
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-24 18:42:56 -0300

    create onclick on heart image for favorite function

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,33 @@
+import { useFavoriteContext } from "contexts/Favorites"
 import styles from "./Card.module.css"
 import favoriteIcon from "./favorite_off.png"
 
 function Card({ id, title, coverImage }) {
-  return (
-    <div className={styles.container}>
 
-        <img 
-            src={coverImage} 
-            alt={title} 
-            className={styles.coverImage}
-        />
+   const { favorite, addFavorite } = useFavoriteContext()
 
-        <h2>{title}</h2>
+   return (
+      <div className={styles.container}>
 
-        <img 
-            src={favoriteIcon} 
-            alt="Icone de Favoritar" 
-            className={styles.favorite}
-        />
+         <img 
+               src={coverImage} 
+               alt={title} 
+               className={styles.coverImage}
+         />
 
-    </div>
-  )
+         <h2>{title}</h2>
+
+         <img 
+               src={favoriteIcon} 
+               alt="Icone de Favoritar" 
+               className={styles.favorite}
+               onClick={() => {
+                  addFavorite({ id, title, coverImage })
+               }}
+         />
+
+      </div>
+   )
 }
 
 
