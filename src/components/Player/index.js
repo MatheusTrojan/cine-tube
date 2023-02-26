c5c95437b93402a7324241709f2cfaebffafc3e2
Matheus Trojan <98136396+MatheusTrojan@users.noreply.github.com>
2023-02-25 21:36:08 -0300

    Create Player component

---
+++ src/components/Player/index.js
@@ -0,0 +1,16 @@
+import styles from "./Player.module.css";
+import Banner from "components/Banner"
+import Title from "components/Title"
+
+function Player() {
+  return (
+    <>
+      <Banner image="player" />
+      <Title>
+        <h1>Player</h1>
+      </Title>
+    </>
+  )
+}
+
+export default PLayer;
