22f388ac20c59ad34bb97bd08c08261c395ee8f3
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-26 22:11:59 -0300

    configure video on player page

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -23,7 +23,7 @@ function Player() {
                height="100%" 
                src={video.link}
                title={video.title}
-               frameborder="0"
+               frameBorder="0"
                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                />
          </section>
