05b68cd1fc518c070f4347cd80139bca1fae730d
Matheus Trojan <matheustrojan23@gmail.com>
2023-02-28 11:17:09 -0300

    created nested routes

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,22 @@
 import Favorites from "pages/Favorites";
 import InitialPage from "pages/InitialPage";
-import Header from "components/Header";
-import Footer from "components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Container from "components/Container";
-import FavoritesProvider from "contexts/Favorites";
 import Player from "pages/Player";
 import NotFound from "pages/NotFound";
+import BasePage from "pages/BasePage";
 
 
 function AppRoutes() {
     return (
         <BrowserRouter>
-            <Header />
-
-            <Container>
-                <FavoritesProvider>
-
                     <Routes>
-                        <Route path="/" element={<InitialPage />}></Route>
-                        <Route path="/favoritos" element={<Favorites />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                        <Route path="*" element={<NotFound />}></Route>
+                        <Route path="/" element={<BasePage />}>
+                            <Route index element={<InitialPage />} />
+                            <Route path="favoritos" element={<Favorites />} />
+                            <Route path=":id" element={<Player />} />
+                            <Route path="*" element={<NotFound />} />
+                        </Route>
                     </Routes>
-                    
-                </FavoritesProvider>
-            </Container>
-
-            <Footer />
         </BrowserRouter>
     )
 }
